4d000d434fe8be0cb43b443f3482ddf4a17e0581
tsdcoffice <tsdcoffice3@gmail.com>
2026-03-02 14:43:05 +0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 REQUIRED for GitHub Pages repo deployment
+  base: "/tsdc_monitoring/",
+
   plugins: [
     react(),
     legacy()
   ],
-  base:"tsdc_monitoring",
+
   test: {
     globals: true,
     environment: 'jsdom',
